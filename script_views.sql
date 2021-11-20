105ad0ed11ce6f632b6222f50221a72680647143
honorinechantre <honorine.chantre@etu.univ-tours.fr>
2021-11-20 18:28:46 -0500

    View

--- script_views.sql
+++ script_views.sql
@@ -93,3 +93,24 @@ JOIN "Division" D on D."noDivision" = E."noDivision"
 JOIN "MatchDeSoccer" MDS on MDS."noMatch" = R."noMatch"
 JOIN "TypeDeMatch" TDM on TDM."noType" = MDS."noType"
 ;
+
+CREATE VIEW EquipeIndisponibiliteFact AS
+    SELECT "nomEquipe",
+           "Equipe"."noIndisponibilite",
+           "NomSaison"."nom" AS "nomSaison",
+           "Division"."nom" AS "nomDivision"
+    FROM "Equipe"
+    JOIN "Indisponibilite" on "Indisponibilite"."noIndisponibilite" = "Equipe"."noIndisponibilite"
+    JOIN "Saison" on "Saison"."noSaison" = "Equipe"."noSaison"
+    JOIN "NomSaison" on "NomSaison"."noNomSaison" = "Saison"."noNomSaison"
+    JOIN "Division" on "Division"."noDivision" = "Equipe"."noDivision";
+
+CREATE VIEW HoraireMatchFact AS
+    SELECT "Equipe"."nomEquipe",
+           "dateMatch",
+           "TypeDeMatch"."nomType",
+           "NomSaison"."nom" AS "nomSaison"
+    FROM "MatchDeSoccer"
+    JOIN "Saison" on "Saison"."noSaison" = "Equipe"."noSaison"
+    JOIN "NomSaison" on "NomSaison"."noNomSaison" = "Saison"."noNomSaison"
+    JOIN "TypeDeMatch" on "TypeDeMatch"."noType" = "MatchDeSoccer"."noType";
