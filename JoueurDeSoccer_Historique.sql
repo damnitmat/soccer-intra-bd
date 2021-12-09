360a2a0ba2c008e6a7ab0f30c386c28b470d1d36
Mathias <mathias.decaille@usherbrooke.ca>
2021-12-08 19:14:22 -0500

    correction camel_case JoueurHistorique

--- JoueurDeSoccer_Historique.sql
+++ JoueurDeSoccer_Historique.sql
@@ -6,32 +6,35 @@ CREATE TABLE "Joueur"(
     "sexe" VARCHAR(1) NOT NULL,
     "courriel" VARCHAR(80) NOT NULL,
     "periode" TSTZRANGE DEFAULT tstzrange(current_timestamp, NULL),
-     PRIMARY KEY("noJoueur")
+    PRIMARY KEY("noJoueur")
 );
 
-CREATE TABLE "JoueurHistorique" AS TABLE  "Joueur";
-CREATE OR REPLACE FUNCTION "Joueur_copierOldDansHistorique"() RETURNS trigger AS $Joueur_copierOldDansHistorique$
+CREATE TABLE "JoueurHistorique" AS TABLE "Joueur";
+
+CREATE OR REPLACE FUNCTION "joueur_copierOldDansHistorique"() RETURNS trigger AS $joueur_copierOldDansHistorique$
     BEGIN
         INSERT INTO "JoueurHistorique" ("noJoueur", "nom", "prenom", "matricule", "sexe", "courriel", "periode")
         VALUES (OLD."noJoueur", OLD."nom", OLD."prenom", OLD."matricule", OLD."sexe", OLD."courriel", tstzrange(lower(OLD.periode), current_timestamp));
 
         RETURN NULL;
     END;
-$Joueur_copierOldDansHistorique$ LANGUAGE plpgsql;
+$joueur_copierOldDansHistorique$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION "Joueur_beforeUpdate"() RETURNS trigger AS $Joueur_beforeUpdate$
+CREATE OR REPLACE FUNCTION "joueur_beforeUpdate"() RETURNS trigger AS $joueur_beforeUpdate$
     BEGIN
         NEW.periode = tstzrange(current_timestamp, NULL);
         RETURN NEW;
     END;
-$Joueur_beforeUpdate$ LANGUAGE plpgsql;
+$joueur_beforeUpdate$ LANGUAGE plpgsql;
 
 CREATE TRIGGER "Joueur_beforeUpdate"
     BEFORE UPDATE ON "Joueur"
     FOR EACH ROW
-    EXECUTE PROCEDURE "Joueur_beforeUpdate"();
+    EXECUTE PROCEDURE "joueur_beforeUpdate"()
+;
 
 CREATE TRIGGER "Joueur_afterUpdateOrDelete"
     AFTER UPDATE OR DELETE ON "Joueur"
     FOR EACH ROW
-    EXECUTE PROCEDURE "Joueur_copierOldDansHistorique"();
+    EXECUTE PROCEDURE "joueur_copierOldDansHistorique"()
+;
